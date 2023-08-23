60b2810680c01a231c35c67f5e815f1d17a7c5c3
DavronSoftwareEngineer <davron.software.engineer@mail.ru>
2023-08-23 16:23:37 +0500

    React typed olib tashlab saqlandi

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { HomeStyle } from "./style";
-import Typed from 'react-typed';
+// import Typed from 'react-typed';
 
 function Home() {
   return (
     <HomeStyle>
       <div className="wraphom">
         <h4 className="title">Davron Mamatov</h4>
-        <p>I'm <Typed className="title-typed" loop strings={['Front-end(Reactjs) developer.']} typeSpeed={40} /></p>
+        {/* <p>I'm <Typed className="title-typed" loop strings={['Front-end(Reactjs) developer.']} typeSpeed={40} /></p> */}
       </div>
     </HomeStyle>
   );
